4c51208bfa00aa69000d57a0065bafcf62ec1ff0
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-10-20 01:15:59 +0300

    charts stub class created

---
+++ src/xyChart.ts
@@ -0,0 +1,50 @@
+export class XYChart {
+    constructor(...args) {
+        console.log('charts not impl');
+    }
+
+    setData(...args) {
+    }
+
+    setMinY(...args) {
+    }
+
+    setMaxY(...args) {
+    }
+
+    setMinX(...args) {
+    }
+
+    setMaxX(...args) {
+    }
+
+    showXAxis(...args) {
+    }
+
+    showYAxis(...args) {
+    }
+
+    setPointSize(...args) {
+    }
+
+    setLineWidth(...args) {
+    }
+
+    setXAxisLabel(...args) {
+    }
+
+    setYFormat(...args) {
+    }
+
+    setXFormat(...args) {
+    }
+
+    setLineColour(...args) {
+    }
+
+    setPointColour(...args) {
+    }
+
+    draw(...args) {
+    }
+}
