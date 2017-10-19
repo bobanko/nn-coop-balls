cd9203b378b2c3b7e8ad96d15e4d56f6b5fd2a41
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-10-20 01:16:30 +0300

    more helpers added

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,21 +2,36 @@
 export const jsPageHeight = 100;
 
 //todo: impl
-export function distance(x1:number, y1:number, x2:number, y2:number):number{
-    return 0; //distance between points
+export function distance(x1: number, y1: number, x2: number, y2: number): number {
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 }
 
-//todo: impl
-export function random(min:number,max:number):number{
+export function random(min: number = 0, max: number = 1): number {
     //range random
-    return 0;
+    return min + Math.random() * (max - min);
 }
 
-//todo: rewrite?
-export function limit(value:number, max:number, min:number):number {
-    if (value < min)
-        return min;
-    if (value > max)
-        return max;
+export function limit(value: number, max: number, min: number): number {
+    if (value < min) return min;
+    if (value > max) return max;
+    return value;
+}
+
+
+export function arraySum(arr: number[]): number {
+    return arr.reduce((sum, item) => sum + item, 0);
+}
+
+
+export function createArray(length: number, value = 0): number[] {
+    return Array(length).fill(value);
+}
+
+export function createMatrix(rows: number, cols: number, value = 0): number[][] {
+    return createArray(rows).map(() => createArray(cols, value));
+}
+
+
+export function nf(value: number, ...args) {
     return value;
 }
