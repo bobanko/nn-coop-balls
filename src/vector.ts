075e8637bf38996c296fde251585c9a88e9d5fe8
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-10-21 15:14:25 +0300

    vector class created with bunch of methods

---
+++ src/vector.ts
@@ -0,0 +1,30 @@
+type TVector = { x: number, y: number };
+export class Vector {
+	static zero = new Vector(0, 0);
+	static one = new Vector(1, 1);
+
+	static distance(vector1: TVector, vector2: TVector): number {
+		return Math.sqrt((vector1.x - vector2.x) ** 2 + (vector1.y - vector2.y) ** 2);
+	}
+
+	constructor(public x: number = 0, public y: number = 0) {
+
+	}
+
+	normalize(): Vector {
+		let dist = this.distance(Vector.zero);
+		return this.multiply(1 / dist);
+	};
+
+	add(vector: TVector): Vector {
+		return new Vector(this.x + vector.x, this.y + vector.y);
+	}
+
+	multiply(multiplier: number): Vector {
+		return new Vector(this.x * multiplier, this.y * multiplier);
+	}
+
+	distance(vector: TVector): number {
+		return Vector.distance(this, vector);
+	}
+}
