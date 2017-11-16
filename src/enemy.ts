f70cd8f52e9384548b9bd2c49da6cc0388f7bdcd
Vladimir Mishko <vladimir.mishko@namecheap.com>
2017-11-16 13:38:37 +0200

    enemy cleanup/refac

--- src/enemy.ts
+++ src/enemy.ts
@@ -1,6 +1,6 @@
 import { Defender } from './defender';
 import { Circle } from './circle';
-import { TVector, Vector } from './vector';
+import { TVector } from './vector';
 
 const color = '#fae596';
 
@@ -8,17 +8,9 @@ export class Enemy extends Circle {
 
 	constructor(position: TVector, radius: number) {
 		super(position, radius, color);
-
-		console.log('oy');
 	}
 
 	intersect(team: Array<Defender>): boolean {
-		//todo: use .any()
-		for (let i = 0; i < team.length; i++) {
-			let dist = Vector.distance(this.position, team[i].position);
-			if (dist < (this.radius + team[i].radius))
-				return true;
-		}
-		return false;
+		return team.some(def => this.intersects(def));
 	}
 }
