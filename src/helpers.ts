711df7503de95d938471c4434a438249e1e8b3d8
Vladimir Mishko <vladimir.mishko@namecheap.com>
2017-11-17 18:32:48 +0200

    createArray/matrix initializer made as func instead of value

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,24 +4,24 @@ export const boxBounds = {start: 300, end: 800};
 
 
 export function random(min: number = 0, max: number = 1): number {
-    //range random
-    return min + Math.random() * (max - min);
+	//range random
+	return min + Math.random() * (max - min);
 }
 
 export function limit(value: number, max: number, min: number): number {
-    if (value < min) return min;
-    if (value > max) return max;
-    return value;
+	if (value < min) return min;
+	if (value > max) return max;
+	return value;
 }
 
 export function arraySum(arr: number[]): number {
-    return arr.reduce((sum, item) => sum + item, 0);
+	return arr.reduce((sum, item) => sum + item, 0);
 }
 
-export function createArray(length: number, value = 0): number[] {
-    return Array(length).fill(value);
+export function createArray(length: number, valueFn = () => 0): number[] {
+	return Array(length).fill(null).map(valueFn);
 }
 
-export function createMatrix(rows: number, cols: number, value = 0): number[][] {
-    return createArray(rows).map(() => createArray(cols, value));
+export function createMatrix(rows: number, cols: number, valueFn = () => 0): number[][] {
+	return createArray(rows).map(() => createArray(cols, valueFn));
 }
